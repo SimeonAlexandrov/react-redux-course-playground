eda95a96a465e273877e3114606fc18f38a7c600
Simeon Aleksandrov <simeon@linux.local>
2018-05-29 17:08:59 +0300

    Restructured project and added react-redux simple book selection app.

---
+++ src/redux/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function BooksReducer() {
+    return [
+        {title: 'dark tower', pages: 100},
+        {title: 'game of thrones', pages: 120},
+        {title: '1984', pages: 1053},
+        {title: 'js', pages: 140},
+    ]
+}
